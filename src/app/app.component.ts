0caf8c353df0aa4903ae2728482f6df5d283f45c
Gerrit Balster <Gerrit.Balster@fiduciagad.de>
2021-06-22 16:06:35 +0200

    Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    // animation triggers go here
+  ]
 })
 export class AppComponent {
   title = 'Angular Test';
